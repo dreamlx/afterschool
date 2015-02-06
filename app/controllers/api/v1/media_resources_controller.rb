766d6d1fe1828e1793dca4f88973fde4b02cd260
ZengWeimao <so.zengtao@gmail.com>
2015-02-06 14:52:27 +0800

    资源的APi

---
+++ app/controllers/api/v1/media_resources_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::MediaResourcesController < Api::V1::Base
+  respond_to :json
+
+  def index
+    @media_resources = MediaResource.paginate(:page => page, :per_page => 12)
+
+    render json: { media_resources: @media_resources }, status: 200
+  end
+
+  def show 
+    @media_resource = MediaResource.find(params[:id])
+
+    render json: { media_resource: @media_resource }, status: 200
+  end
+
+  def create
+    @media_resource = MediaResource.new(media_resource_params)
+
+    if @media_resource.save
+      render json: { media_resource: @media_resource }, status: 200
+    else
+      render json: { media_resource: { error: "创建资源失败"} }, status: 401
+    end
+  ensure
+    clean_tempfile
+  end
+
+  def update
+    @media_resource = MediaResource.find(params[:id])
+    if @media_resource.update(media_resource)
+      render json: { media_resource: @media_resource }, status: 200
+    else
+      render json: { media_resource: { error: "更新资源失败" }}, status: 401
+    end
+  end
+
+  def destroy 
+    @media_resource = MediaResource.find(params[:id])
+    @media_resource.destroy!
+
+    render json: { media_resource: @media_resource }, status: 204
+  end
+
+  private
+
+  def media_resource_params
+    _the_params = params.require(:media_resource).permit(:description, :avatar, :work_paper_id)
+    _the_params[:avatar] = parse_data(_the_params[:avatar]) if _the_params[:avatar]
+    _the_params
+  end
+end
--- app/controllers/api/v1/work_papers_controller.rb
+++ app/controllers/api/v1/work_papers_controller.rb
@@ -61,32 +61,5 @@ class Api::V1::WorkPapersController < Api::V1::BaseController
     end
     return false #TODO应该返回 teacher not found or unactive的消息
   end
-
-  def parse_data(base64_image)
-    in_content_type, encoding, string = base64_image.split(/[:;,]/)[1..3]
-    #{暂时先这样}
-    filename = "upload-data"
-
-    @tempfile = Tempfile.new(filename)
-    @tempfile.binmode
-    @tempfile.write Base64.decode64(string)
-    @tempfile.rewind
-
-    content_type = `file --mime -b #{@tempfile.path}`.split(";")[0]
-    extension = content_type.match(/gif|jpeg|png｜mp3|mp4|mov/).to_s
-    filename += ".#{extension}" if extension
-
-    ActionDispatch::Http::UploadedFile.new({
-      tempfile: @tempfile,
-      content_type: content_type,
-      filename: filename
-    })
-  end
-
-  def clean_tempfile
-    if @tempfile
-      @tempfile.close
-      @tempfile.unlink
-    end
-  end
+  
 end
