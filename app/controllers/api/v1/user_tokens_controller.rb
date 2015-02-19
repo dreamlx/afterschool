297f9452e8504a3dae3a7cf406ae9b8472bd607f
dreamlx <dreamlinx@gmail.com>
2015-02-19 13:42:56 +0800

    fix api/user

--- app/controllers/api/v1/user_tokens_controller.rb
+++ app/controllers/api/v1/user_tokens_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::UserTokensController  < Api::V1::BaseController
   respond_to :json
-
   def index
-
+    render json: { message: 'get token: /api/v1/user_tokens/:id' }, status: 200
   end
 
   # 登陆就是验证token的过程
@@ -14,15 +13,28 @@ class Api::V1::UserTokensController  < Api::V1::BaseController
       sign_in("user", user)
       user.ensure_authentication_token
 
-      render json: { token: user.authentication_token, user_id: user.id }
+      render json: { token: user.authentication_token, user_id: user.id, user: user }
     else
       render json: { error: { status: -1 } }
     end
   end
 
   def destroy
-    current_user.authentication_token = Devise.friendly_token
-    sign_out(current_user)
-    render json: { success: true }
+    @user = User.find_by(:id => params[:id])
+
+    if @user.nil?
+      logger.info("Token not found.")
+      render :status => 404, :json => { :error=>"Invalid token" }
+    else
+      @user.reset_authentication_token!
+      render json: { success: true }
+    end
+
+  end
+
+    private
+  def user_params
+    the_params = params.require(:user).permit(:email, :password, :password_confirmation, :role, profile_attributes: [])
+    return the_params
   end
 end
