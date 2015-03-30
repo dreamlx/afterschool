b5ac45f2cb6869475c157ac7ecd707bf9e989026
chenge <chenge2k@gmail.com>
2015-03-30 17:59:27 +0800

    f335 fixed

--- app/admin/user_message.rb
+++ app/admin/user_message.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register UserMessage do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :topic, :body, :message_type
+  #permit_params :topic, :body, :message_type
   #
   # or
   #
@@ -19,10 +19,14 @@ ActiveAdmin.register UserMessage do
     column :topic
     column :body
     column :received_user do |m|
-      link_to User.find(m.received_messageable_id).nickname, admin_user_path(m.received_messageable_id)
+      if m.received_messageable_id
+        link_to User.find(m.received_messageable_id).nickname, admin_user_path(m.received_messageable_id)
+      end
     end
     column :sent_user do |m|
-      link_to User.find(m.sent_messageable_id).nickname, admin_user_path(m.sent_messageable_id)
+      if m.sent_messageable_id
+        link_to User.find(m.sent_messageable_id).nickname, admin_user_path(m.sent_messageable_id)
+      end
     end
     column :created_at
     actions
