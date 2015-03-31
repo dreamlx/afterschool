531677f09ccc678e8736cbf4f77c109d3c85d890
chenge <chenge2k@gmail.com>
2015-03-31 15:37:19 +0800

    f336 forum draft

---
+++ app/admin/post.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Post do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  
+  permit_params :title, :body
+  config.comments = true
+  
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  index do
+    column :id 
+    column :title
+    column :body
+    column :created_at
+    actions
+  end
+end
