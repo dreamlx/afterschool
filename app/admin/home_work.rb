3e06c580caf081be6dcf36da0ab9e880e6fbaf5c
chenge <chenge2k@gmail.com>
2015-04-02 13:25:09 +0800

    f336 mp3 type fixed

--- app/admin/home_work.rb
+++ app/admin/home_work.rb
@@ -1,19 +1,5 @@
 ActiveAdmin.register HomeWork do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
   permit_params :title, 
     :description, 
     :student_id, 
@@ -21,8 +7,6 @@ ActiveAdmin.register HomeWork do
     :state, 
     media_resources_attributes: [:id, :avatar, :_destroy,:description],
     work_review_attributes: [:id, :rate, :remark, :home_work_id, :teacher_id]
-
-
 
   index do
     column  :id 
--- app/admin/user_message.rb
+++ app/admin/user_message.rb
@@ -1,20 +1,6 @@
 ActiveAdmin.register UserMessage do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  
   permit_params :topic, :body, :message_type
-  
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
 
   index do
     column :id 
