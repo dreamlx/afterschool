83f12511401d64aea068276da5d48f06447ebf7f
chenge <chenge2k@gmail.com>
2015-06-03 17:40:12 +0800

    inform

--- app/controllers/api/v1/informs_controller.rb
+++ app/controllers/api/v1/informs_controller.rb
@@ -1,18 +1,39 @@
 class Api::V1::InformsController < Api::V1::BaseController
 
   respond_to :json
-  
-  inherit_resources
 
-  # def index
-  #   @informs = Inform.all
-  #   render json: { informs: @informs }
-  # end
+  def index
+    @informs = Inform.where("teacher_id=#{params[:teacher_id]} and school_class_id=#{params[:school_class_id]} ")
+    render json: @informs
+  end
+
+  def update
+    @inform = Inform.find(params[:id])
+    @inform.update!(inform_params)
+    render_msg 'ok'
+  end
+
+  def create
+    @inform = Inform.new(inform_params)
+    @inform.save!
+    render_msg 'ok'
+  end
+
+  def show
+    @inform = Inform.find(params[:id])
+    render json: @inform
+  end
+
+  def destroy
+    @inform = Inform.find(params[:id])
+    @inform.destroy!
+    render_msg 'ok'
+  end
 
   private
 
-    def inform_params
-      params.require(:inform).permit!
-    end
+  def inform_params
+    params.require(:inform).permit!
+  end
 
 end
