f31cb3b88bc97ef813bdd9cf6c849309cd6ca9fe
chenge <chenge2k@gmail.com>
2015-06-03 16:36:33 +0800

    inherited

--- app/controllers/api/v1/informs_controller.rb
+++ app/controllers/api/v1/informs_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::InformsController < Api::V1::BaseController
 
   respond_to :json
-
+  
   inherit_resources
 
-  def index
-    @informs = Inform.all
-    render json: { informs: @informs }
-  end
+  # def index
+  #   @informs = Inform.all
+  #   render json: { informs: @informs }
+  # end
 
   private
 
     def inform_params
-      params.require(:informs).permit!
+      params.require(:inform).permit!
     end
 
 end
