60a8d0bac34aca2ecde7423aadcd3a1f9ed81717
dreamlx <dreamlinx@gmail.com>
2015-09-07 11:16:14 +0800

    import

--- lib/tasks/import.rb
+++ lib/tasks/import.rb
@@ -1,13 +1,17 @@
 email = 200
 
-CSV.foreach('db/12.csv') do |row|
+CSV.foreach('db/13.csv') do |row|
 	u = Student.new
 	u.nickname = row[2]
-	u.email = '20150503'+email.to_s+'@qq.com'
+	u.email = '20150906'+email.to_s+'@qq.com'
    u.role = 'student'
-   u.school_class_id = 4
+   u.school_class_id = 9
    u.password = '11111111'
    
    u.save
    email = email +1
+
+   u.profile.gender = row[3]
+   u.profile.class_number = row[1]
+   u.save
 end
