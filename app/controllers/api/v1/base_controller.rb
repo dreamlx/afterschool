aa3d28cdaee74a56df53c7e7b376142657e5d1c0
ken-wong <wangkun025@gmail.com>
2020-09-12 17:04:23 +0800

    Fix cap

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -142,7 +142,7 @@ class Api::V1::BaseController < ApplicationController
   protected
 
   def paged(customer, per_page = 12 )
-    customer.paginate(:page => params[:page], :per_page => per_page)
+    customer.page(params[:page]).per(per_page)
   end
 
   private
@@ -157,31 +157,3 @@ class Api::V1::BaseController < ApplicationController
 
 
 end
-
-  # def parse_data(base64_image)
-  #   in_content_type, encoding, string = base64_image.split(/[:;,]/)[1..3]
-  #   #{暂时先这样}
-  #   filename = "upload-data"
-
-  #   @tempfile = Tempfile.new(filename)
-  #   @tempfile.binmode
-  #   @tempfile.write Base64.decode64(string)
-  #   @tempfile.rewind
-
-  #   content_type = `file --mime -b #{@tempfile.path}`.split(";")[0]
-  #   extension = content_type.match(/gif|jpeg|png｜mp3|mp4|mov/).to_s
-  #   filename += ".#{extension}" if extension
-
-  #   ActionDispatch::Http::UploadedFile.new({
-  #     tempfile: @tempfile,
-  #     content_type: content_type,
-  #     filename: filename
-  #   })
-  # end
-
-  # def clean_tempfile
-  #   if @tempfile
-  #     @tempfile.close
-  #     @tempfile.unlink
-  #   end
-  # end
--- app/controllers/api/v1/school_classes_controller.rb
+++ app/controllers/api/v1/school_classes_controller.rb
@@ -5,17 +5,10 @@ class Api::V1::SchoolClassesController < Api::V1::BaseController
 
     if params[:teacher_id]
       classes = Teacher.find(params[:teacher_id]).school_classes
-      @classes = classes.paginate(:page => params[:page], :per_page => 60)
+      @classes = classes.page(params[:page]).per(60)
     else
-      @classes = SchoolClass.paginate(:page => params[:page], :per_page => 60) 
-    end
-
-    # classes = Teacher.find(params[:teacher_id]).school_classes unless params[:teacher_id].blank?
-    # if classes.nil?
-    #   @classes = SchoolClass.paginate(:page => params[:page], :per_page => 60) 
-    # else
-    #   @classes = classes.paginate(:page => params[:page], :per_page => 60)
-    # end    
+      @classes = SchoolClass.page(params[:page]).per(60)
+    end   
   
     render json: { school_classes: @classes }
   end
